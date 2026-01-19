f0350273eeff719d239b5e87c6002eff2417554d
WakandianShield <santiagogallardojr@gmail.com>
2026-01-19 17:01:47 +0000

    COMMIT CON FORMULARIO FRONTEND COMPLETO

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const hamburger = document.getElementById("menuToggle");
 const nav = document.querySelector(".link-div");
 const navLinks = document.querySelectorAll(".nav-link");
 
+
+const registerTab = document.getElementById("register-tab");
+const loginTab = document.getElementById("login-tab");
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
+
+
+
+
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   nav.classList.toggle("active");
 });
 
-// Cerrar menú al clickear en un link
+
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     hamburger.classList.remove("active");
@@ -16,11 +26,41 @@ navLinks.forEach(link => {
 });
 
 
-const logIn = document.querySelector(".form-div");
-const btn = document.querySelector(".logIn");
 
-btn.addEventListener("click", () => {
-  // aquí ES verdad que hubo un click
-  logIn.classList.toggle("active");
+
+
+
+function showForm(formToShow) {
+    if(formToShow === "register") {
+        registerTab.classList.add("active");
+        loginTab.classList.remove("active");
+
+        registerForm.style.opacity = 1;
+        registerForm.style.visibility = "visible";
+        loginForm.style.opacity = 0;
+        loginForm.style.visibility = "hidden";
+    } else {
+        loginTab.classList.add("active");
+        registerTab.classList.remove("active");
+
+        loginForm.style.opacity = 1;
+        loginForm.style.visibility = "visible";
+        registerForm.style.opacity = 0;
+        registerForm.style.visibility = "hidden";
+    }
+}
+
+registerTab.addEventListener("click", () => showForm("register"));
+loginTab.addEventListener("click", () => showForm("login"));
+
+registerForm.addEventListener("submit", e => {
+    e.preventDefault();
+    alert("Registro enviado!");
 });
 
+loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    alert("Login enviado!");
+});
+
+window.addEventListener("DOMContentLoaded", () => showForm("register"));
